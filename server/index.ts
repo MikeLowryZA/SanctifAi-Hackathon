f8a07c29ff4b5d05c549bd8c394a2fbb746e4802
MikeLowry021 <stan1.baby@gmail.com>
2025-11-25 11:33:31 +0200

    Updated index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+
+console.log("ENV check:", {
+  hasOpenAI: !!process.env.OPENAI_API_KEY,
+  hasTMDB: !!process.env.TMDB_API_KEY,
+});
 
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
